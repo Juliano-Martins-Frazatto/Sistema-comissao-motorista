66df5fcc34062611b0190eb130e9ae3f2daf1128
Juliano Martins Frazatto <julianofrazatto2008@gmail.com>
2026-01-21 19:08:30 -0300

    Práticas de organização

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
 reset = "\033[0m"
 verde = "\033[92m"
-vermelho = "\033[91m"
+
+
+def cor_verde(mensagem):
+    print(f"{verde}{mensagem}{reset}")
 
 
 def converter_entrada_de_dados(mensagem):
@@ -12,11 +14,40 @@ def converter_entrada_de_dados(mensagem):
             print("Erro: digite apenas números")
 
 
-print(
-    f"{verde}================================================================\n")
-print("Você iniciou a ferramenta de cálculo de fretes!\n")
-print(
-    f"----------------------------------------------------------------\n{reset}")
+def calculo_frete(valor_frete_ida, valor_frete_volta):
+    resultado = valor_frete_ida + valor_frete_volta
+    return resultado
+
+
+def soma_ped_dinheiro(pedagio_ida_dinheiro, pedagio_volta_dinheiro):
+    resultado = pedagio_ida_dinheiro + pedagio_volta_dinheiro
+    return resultado
+
+
+def soma_ped_tag(pedagio_ida_tag, pedagio_volta_tag):
+    resultado = pedagio_ida_tag + pedagio_volta_tag
+    return resultado
+
+
+def calculo_comissao_bruta(
+    valor_frete_ida,
+    valor_frete_volta,
+    pedagio_ida_tag,
+    pedagio_volta_tag,
+    pedagio_ida_dinheiro,
+    pedagio_volta_dinheiro
+):
+    soma_frete = calculo_frete(valor_frete_ida, valor_frete_volta)
+    soma_despesas = soma_ped_tag(pedagio_ida_tag, pedagio_volta_tag) + \
+        soma_ped_dinheiro(pedagio_ida_dinheiro, pedagio_volta_dinheiro)
+
+    porcentagem = (soma_frete - soma_despesas) * 12
+
+    conta_comissao = porcentagem / 100
+    return conta_comissao
+
+
+cor_verde("============================================================\n       \nBem-vindo\n                        \n============================================================")
 
 while True:
     valor_frete_ida = converter_entrada_de_dados(
@@ -25,13 +56,8 @@ while True:
     valor_frete_volta = converter_entrada_de_dados(
         f"\nValor do frete de volta (verificar se o vale pedágio não está incluso, se estiver, subtraia o valor do mesmo!):")
 
-    soma_frete_ida_e_volta = valor_frete_ida + valor_frete_volta
-
-    print(
-        f"{verde}\n============================================================")
-    print(f"\nValor do frete ida + frete volta: {soma_frete_ida_e_volta}")
-    print(
-        f"\n============================================================{reset}")
+    cor_verde(
+        f"============================================================\n           Valor do frete ida + frete volta: {calculo_frete(valor_frete_ida, valor_frete_volta)}    \n============================================================")
 
     pedagio_ida_dinheiro = converter_entrada_de_dados(
         f"\nPedágio de ida no dinheiro:")
@@ -44,49 +70,36 @@ while True:
     pedagio_volta_tag = converter_entrada_de_dados(
         f"\nPedágio de volta na TAG:")
 
-    soma_ped_ida_e_volta_dinheiro = pedagio_ida_dinheiro + pedagio_volta_dinheiro
-
-    soma_ped_ida_e_volta_tag = pedagio_ida_tag + pedagio_volta_tag
-
-    print(
-        f"{verde}\n============================================================")
-    print(
-        f"\nSoma dos pedágios de ida e volta no dinheiro:{soma_ped_ida_e_volta_dinheiro}")
-
-    print(
-        f"\nSoma dos pedágios de ida e volta na TAG:{soma_ped_ida_e_volta_tag}{reset}")
+    cor_verde(f"{verde}\n============================================================       \nSoma dos pedágios de ida e volta no dinheiro:{soma_ped_dinheiro(pedagio_ida_dinheiro, pedagio_volta_dinheiro)}                    \nSoma dos pedágios de ida e volta na TAG:{soma_ped_tag(pedagio_ida_tag, pedagio_volta_tag)}\n============================================================{reset}")
 
     vale_viagem = converter_entrada_de_dados(f"\nVale para viagem:")
 
     outras_despesas_dinheiro = converter_entrada_de_dados(
         f"\nOutras despesas no dinheiro(será descontado do vale):")
 
-    conta_comissao = (
-        ((soma_frete_ida_e_volta - (soma_ped_ida_e_volta_tag + soma_ped_ida_e_volta_dinheiro)) * 12)/100)
-    print(
-        f"{verde}\n============================================================")
-    print(f"\nComissão bruta:{conta_comissao}")
-    print(
-        f"\n============================================================{reset}")
+    comissao_bruta = calculo_comissao_bruta(
+        valor_frete_ida,
+        valor_frete_volta,
+        pedagio_ida_tag,
+        pedagio_volta_tag,
+        pedagio_ida_dinheiro,
+        pedagio_volta_dinheiro
+    )
+    cor_verde(
+        f"{verde}\n============================================================\nComissão bruta:{comissao_bruta}\n============================================================{reset}")
 
     conta_vale = vale_viagem - outras_despesas_dinheiro
 
     if conta_vale < 0:
-        print("===Vale insuficiente===")
 
         conta_vale_insuficiente = conta_vale * -1
 
-        print("O valor excedido do vale será pago junto com a comissão!")
-
-        conta_comissao_final_vale_insuficiente = conta_comissao + conta_vale_insuficiente
-
-        print(
-            f"\nPagar para o motorista: {conta_comissao_final_vale_insuficiente}")
+        conta_comissao_final_vale_insuficiente = comissao_bruta + conta_vale_insuficiente
+        cor_verde(
+            f"vale insuficiente===\n                                                     O valor excedido do vale será pago junto com a comissão!\n==================================================================\nPagar para o motorista: {conta_comissao_final_vale_insuficiente}\n===============================================================          ")
 
     else:
-        print(
-            f"\nSobrou {conta_vale} de vale. O valor será descontado da comissão")
+        conta_vale_suficiente = comissao_bruta - conta_vale
 
-        conta_vale_suficiente = conta_comissao - conta_vale
-
-        print(f"\nPagar para o motorista:{conta_vale_suficiente}")
+        cor_verde(
+            f"\n===========================================================    \nSobrou {conta_vale} de vale. Esse valor será descontado da comissão.\nPagar para o motorista:{conta_vale_suficiente}\n===========================================================            ")
